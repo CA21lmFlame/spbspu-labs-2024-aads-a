642df515f9f3930f87fbdd6876b46fc8fb5d575b
CA21lmFlame <chernov.ar@spbstu.ru>
2024-08-25 23:29:24 +0300

    implemented capacity, default constructors and element access

--- chernov.artem/S4/UBSTree.hpp
+++ chernov.artem/S4/UBSTree.hpp
@@ -2,6 +2,7 @@
 #define	UBSTREE_HPP
 #include "treeNode.hpp"
 #include <functional>
+#include <cstddef>
 
 namespace chernov
 {
@@ -9,16 +10,62 @@ namespace chernov
   class UBSTree
   {
   public:
-    UBSTree():
-      root_(nullptr),
-      size_(0),
-      comp_()
-    {}
+    UBSTree();
+    UBSTree(UBSTree&& obj);
+    ~UBSTree();
+
+    size_t size() const;
+    bool empty() const;
+
+    Value& at(const Key& key);
+    const Value& at(const Key& key) const;
+
   private:
     TreeNode< Key, Value >* root_;
     size_t size_;
     Compare comp_;
   };
+
+  template < typename Key, typename Value, typename Compare >
+  UBSTree< Key, Value, Compare >::UBSTree():
+    root_(nullptr),
+    size_(0),
+    comp_()
+  {}
+
+  template < typename Key, typename Value, typename Compare >
+  UBSTree< Key, Value, Compare >::UBSTree(UBSTree&& obj):
+    root_(obj.root_),
+    size_(obj.size_),
+    comp_(obj.comp_)
+  {
+    obj.root_ = nullptr;
+    obj.size_ = 0;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  UBSTree< Key, Value, Compare >::~UBSTree()
+  {}
+
+  template < typename Key, typename Value, typename Compare >
+  size_t UBSTree< Key, Value, Compare >::size() const
+  {
+    return size_;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  bool UBSTree< Key, Value, Compare >::empty() const
+  {
+    return size_ == 0;
+  }
+
+  template < typename Key, typename Value, typename Compare >
+  Value& UBSTree< Key, Value, Compare >::at(const Key& key)
+  {}
+
+  template < typename Key, typename Value, typename Compare >
+  const Value& UBSTree< Key, Value, Compare >::at(const Key& key) const
+  {}
 }
 
 #endif
